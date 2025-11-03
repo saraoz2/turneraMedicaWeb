52dafa19b5d7c05922445692fcf53c5194a258e7
Santiago Araoz <santiagoaraoz@gmail.com>
2025-11-03 09:49:24 -0300

    agrego metodos PUT, PATCH y DELETE

--- src/controllers/pacientes.controller.js
+++ src/controllers/pacientes.controller.js
@@ -30,6 +30,41 @@ export const createPaciente = async (req, res)=> {
     })
 }
 
-export const updatePaciente = (req, res)=> res.send('actualizando pacientes')
+export const updatePaciente = async (req, res)=> {
+    const {id} = req.params
+    const {nombre, apellido, telefono, dni, fecha_nac} = req.body
 
-export const deletePaciente = (req, res)=> res.send('eliminando pacientes')
+    const [result] = await pool.query('UPDATE paciente SET nombre =?, apellido =?, telefono =?, dni =?, fecha_nac =? WHERE id = ?',[nombre, apellido, telefono, dni, fecha_nac, id])
+    if (result.affectedRows == 0) return res.status(404).json({
+        message: 'paciente no encontrado'
+    })
+
+    const [rows] = await pool.query('SELECT * FROM paciente WHERE id = ?',[id])
+    res.json(rows[0])
+
+}
+
+export const updatePatchPaciente = async (req, res)=> {
+    const {id} = req.params
+    const {nombre, apellido, telefono, dni, fecha_nac} = req.body
+
+    const [result] = await pool.query('UPDATE paciente SET nombre =IFNULL(?, nombre), apellido =IFNULL(?, apellido), telefono =IFNULL(?, telefono), dni =IFNULL(?, dni), fecha_nac =IFNULL(?, fecha_nac) WHERE id = ?',[nombre, apellido, telefono, dni, fecha_nac, id])
+    if (result.affectedRows == 0) return res.status(404).json({
+        message: 'paciente no encontrado'
+    })
+
+    const [rows] = await pool.query('SELECT * FROM paciente WHERE id = ?',[id])
+    res.json(rows[0])
+
+}
+
+export const deletePaciente = async (req, res)=> {
+    console.log(req.params.id)
+    const [result] = await pool.query('DELETE FROM paciente WHERE id = ?',[req.params.id])
+    if (result.affectedRows <= 0) return res.status(404).json({
+        message: 'paciente no encontrado'
+    })
+
+    res.send(204)
+
+}
--- src/routes/pacientes.routes.js
+++ src/routes/pacientes.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
-import {getPacientes, createPaciente, updatePaciente, deletePaciente, getPaciente} from '../controllers/pacientes.controller.js'
+import {getPacientes, createPaciente, updatePaciente, deletePaciente, getPaciente, updatePatchPaciente} from '../controllers/pacientes.controller.js'
 
 const router = Router()
 
 router.get('/paciente', getPacientes)
 router.get('/paciente/:id', getPaciente)
 router.post('/paciente', createPaciente)
-router.put('/paciente', updatePaciente)
-router.delete('/paciente', deletePaciente)
+router.put('/paciente/:id', updatePaciente)
+router.patch('/paciente/:id', updatePatchPaciente)
+router.delete('/paciente/:id', deletePaciente)
 
 export default router
